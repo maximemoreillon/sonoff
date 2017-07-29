4d7397e00dc80d2c5706b7f4974fcf9b552dc964
Maxime Moreillon <m.moreillon@gmail.com>
2017-07-30 00:05:19 +0900

    Moved config files to external folder

--- bedroom_light_config.h
+++
@@ -1,9 +0,0 @@
-#define HOSTNAME "bedroomLight"
-
-#define MQTT_COMMAND_TOPIC "light/bedroom/command"
-#define MQTT_STATUS_TOPIC "light/bedroom/status"
-#define MQTT_DEBUG_TOPIC "light/bedroom/debug"
-
-#define RELAY_INITIAL_STATE "ON"
-
-const int banana = 2;
