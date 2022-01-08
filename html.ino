6c2e404a96ba29181daff6747d99ee52555f87bb
Maxime Moreillon <m.moreillon@gmail.com>
2022-01-08 12:03:05 +0900

    json

---
+++ html.ino
@@ -0,0 +1,120 @@
+// Head partials
+// Style
+const String styles = "<style>"
+  ".main{"
+  "padding: 25px;"
+  "text-align: center;"
+  "}"
+  ".status_container{"
+  "display:flex;"
+  "justify-content:center;"
+  "}"
+  ".status{"
+  "width: 35vmin;"
+  "height: 35vmin;"
+  "border-radius: 50%;"
+  "border: 10px solid #ce0000;"
+  "padding: 5px;"
+  "background: white;"
+  "background-clip: content-box;"
+  "transition: background-color 0.5s;"
+  "}"
+  "</style>";
+
+// Body partials
+const String header = "<div class='header'>"
+  + String(HOSTNAME) +
+  "</div>";
+
+const String nav = "<div class='nav'>"
+  "<a href='/'>Status</a>"
+  "<a href='/update_form'>Firmware update</a>"
+  "</div>";
+  
+const String footer= "<div class='footer'>"
+  "<img src='https://maximemoreillon.com/images/logo/logo.png'>"
+  "  <div>"
+  "    <div>SONOFF firmware v1.3</div>"
+  "    <div>Maxime Moreillon</div>"
+  "  </div>"
+  "</div>";
+
+// Update form
+String update_form = "<h2>Firmware update</h2>"
+  "<form method='POST' action='/update' enctype='multipart/form-data'>"
+  "<input type='file' name='update'>"
+  "<input type='submit' value='Update'>"
+  "</form>";
+
+// Combinations
+String head ="<head>"
+  "<title>" + String(HOSTNAME) +"</title>"
+  + styles +
+  "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+  "<link rel='icon' href='https://webcomponents.maximemoreillon.com/logo/logo.png'>"
+  "<link rel='stylesheet' type='text/css' href='https://webcomponents.maximemoreillon.com/master_css/master.css'>"
+  "<link rel='stylesheet' type='text/css' href='https://webcomponents.maximemoreillon.com/modals/modals.css'>"
+  "<link rel='stylesheet' type='text/css' href='https://webcomponents.maximemoreillon.com/loader/loader.css'>"
+  "<script src='https://webcomponents.maximemoreillon.com/loader/loader.js'></script>"
+  "<script src='https://webcomponents.maximemoreillon.com/modals/modals.js'></script>"
+  "</head>";
+
+String pre_main = "<html>"
+  + head + 
+  "<body>"
+  "<div class='wrapper'>"
+  + header + nav +
+  "<div class='main'>";
+  
+String post_main = "</div>"
+  + footer +
+  "</div>"
+  "</body>"
+  "</html>";
+
+// Root
+String root_main = ""
+  "<div class='status_container'>"
+  "  <div class='status' id='status' onclick='toggle()'></div>"
+  "</div>"
+  "<div id='disconnected_modal' class='modal_wrapper'>"
+  "  <div class='modal_container'>"
+  "    NO CONNECTION"
+  "  </div>"
+  "</div>"
+  "<div id='loader' class='loader_wrapper'>"
+  "  <div id='loader_loading' class='loader_loading'></div>"
+  "  <div id='loader_success' class='loader_success'>OK</div>"
+  "  <div id='loader_error' class='loader_error'>ERROR</div>"
+  "</div>"
+  "<script>"
+  "websock = new WebSocket('ws://' + window.location.hostname + ':81/');"
+  "websock.onopen = function(evt) { close_modal_by_ID('disconnected_modal'); };"
+  "websock.onclose = function(evt) { open_modal_by_ID('disconnected_modal'); };"
+  "websock.onerror = function(evt) { console.log(evt); };"
+  "websock.onmessage = function(evt) {"
+  "  var status = document.getElementById('status');"
+  "  if(evt.data == 'ON'){"
+  "    status.style.backgroundColor = '#ce0000';"
+  "  }"
+  " else if(evt.data == 'OFF'){"
+  "    status.style.backgroundColor = '#ffffff';"
+  "  }"
+  "};"
+  ""
+  "function toggle(){"
+  "  open_loader();"
+  "  var xhttp = new XMLHttpRequest();"
+  "  xhttp.open('GET','/toggle',true);"
+  "  xhttp.onreadystatechange = function () {"
+  "    if(xhttp.readyState === 4){"
+  "      if(xhttp.status === 200) close_loader_success();"
+  "      else close_loader_error();"
+  "    }"
+  "  };"
+  "  xhttp.send();"
+  "}"
+  "</script>";
+
+// Not found
+String not_found = "<h2>Not found</h2>";
--- mqtt.ino
+++ mqtt.ino
@@ -1,105 +1,86 @@
 void MQTT_setup(){
-
-  // Callbacks
-  MQTT_client.onConnect(MQTT_connect_callback);
-  MQTT_client.onDisconnect(MQTT_disconnect_callback);
-  MQTT_client.onSubscribe(MQTT_subscribe_callback);
-  MQTT_client.onUnsubscribe(MQTT_unsubscribe_callback);
-  MQTT_client.onMessage(MQTT_message_callback);
-  MQTT_client.onPublish(MQTT_publish_callback);
-
-  // Settings
+  Serial.println(F("[MQTT] MQTT setup"));
+  
   MQTT_client.setServer(MQTT_BROKER_ADDRESS, MQTT_PORT);
-  MQTT_client.setCredentials(MQTT_USERNAME, MQTT_PASSWORD);
-  MQTT_client.setWill(MQTT_STATUS_TOPIC, MQTT_QOS, MQTT_RETAIN, MQTT_LAST_WILL);
+  MQTT_client.setCallback(MQTT_message_callback);
 }
 
-void MQTT_connect() {
-  Serial.println("MQTT connecting...");
+void MQTT_connection_manager(){
 
-  MQTT_reconnect_timer.detach();
-  MQTT_client.connect();
-}
+  static int MQTT_connected = -1; // 1: connected, 0: disconnected, -1: unknown
+  static long last_MQTT_connection_attempt;
+  
+  if(!MQTT_client.connected()) {
+    if(MQTT_connected != 0){
+      // MQTT connection status changed to "disconnected"
+      MQTT_connected = 0;
 
-void MQTT_connect_callback(bool sessionPresent) {
-  Serial.println("MQTT connected");
+      Serial.println(F("[MQTT] Disconnected"));
+    }
+        
+    if(millis() - last_MQTT_connection_attempt > 1000){
+      last_MQTT_connection_attempt = millis();
+      MQTT_client.connect(HOSTNAME, MQTT_USERNAME, MQTT_PASSWORD, MQTT_STATUS_TOPIC, MQTT_QOS, MQTT_RETAIN, MQTT_LAST_WILL);
+    }
+        
+  }
+  else {
+    if(MQTT_connected != 1){
+      // MQTT connection status changed to "connected"
+      MQTT_connected = 1;
 
-  // Subscribing to command topics
-  MQTT_client.subscribe(MQTT_COMMAND_TOPIC, MQTT_QOS);
-
-  // Update status
-  MQTT_client.publish(MQTT_STATUS_TOPIC, MQTT_QOS, MQTT_RETAIN, relay_state);
-}
-
-void MQTT_disconnect_callback(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("MQTT disconnected");
-
-  if (WiFi.isConnected()) {
-    MQTT_reconnect_timer.attach(2, MQTT_connect);
+      Serial.println(F("[MQTT] Connected"));
+      
+      Serial.println(F("[MQTT] Subscribing to topics"));
+      MQTT_client.subscribe(MQTT_COMMAND_TOPIC);
+      
+      MQTT_publish_state();
+    }
   }
 }
 
-void MQTT_subscribe_callback(uint16_t packetId, uint8_t qos) {
-  Serial.println("MQTT subscribed successfully");
+void MQTT_publish_state(){
+  // Send the relay state through MQTT
+  Serial.println(F("[MQTT] publish of relay state"));
+  
+  // Create a JSON document object
+  StaticJsonDocument<200> outbound_JSON_message;
+
+  // Assign the relay state to the state key of the JSON payload
+  outbound_JSON_message["state"] = relay_state;
+
+  // Prepare a buffer to send the JSON object
+  char JSONmessageBuffer[100];
+  serializeJson(outbound_JSON_message, JSONmessageBuffer, sizeof(JSONmessageBuffer));
+  
+  //Send the payload
+  MQTT_client.publish(MQTT_STATUS_TOPIC, JSONmessageBuffer, MQTT_RETAIN);
 }
 
-void MQTT_unsubscribe_callback(uint16_t packetId) {
-  Serial.println("MQTT unsubscribed successfully");
-}
 
-void MQTT_message_callback(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
 
-  Serial.print("MQTTT message received: ");
-  Serial.print("  topic: ");
+void MQTT_message_callback(char* topic, byte* payload, unsigned int length) {
+
+  // Debugging
+  Serial.print(F("[MQTTT] message received on "));
   Serial.print(topic);
-  Serial.print("  payload: ");
-  Serial.print(payload);
-  Serial.print("  qos: ");
-  Serial.print(properties.qos);
-  Serial.print("  dup: ");
-  Serial.print(properties.dup);
-  Serial.print("  retain: ");
-  Serial.print(properties.retain);
-  Serial.print("  len: ");
-  Serial.print(len);
-  Serial.print("  index: ");
-  Serial.print(index);
-  Serial.print("  total: ");
-  Serial.println(total);
+  Serial.print(F(", payload: "));
+  for (int i = 0; i < length; i++) Serial.print((char)payload[i]);
+  Serial.println();
 
-  if(strncmp(payload, "ON", len) == 0 || strncmp(payload, "on", len) == 0){
-    Serial.println("Relay ON");
-    digitalWrite(RELAY_PIN, HIGH);
-    digitalWrite(LED_PIN, LOW); // LED is active LOW
-    relay_state = "ON";
-  }
-  else if(strncmp(payload, "OFF", len) == 0 || strncmp(payload, "off", len) == 0){
-    Serial.println("Relay OFF");
-    digitalWrite(RELAY_PIN, LOW);
-    digitalWrite(LED_PIN, HIGH); // LED is active LOW
-    relay_state = "OFF";
-  }
-  else if(strncmp(payload, "TOGGLE", len) == 0){
-    Serial.println("Toggling relay state");
-    
-    if(strcmp(relay_state,"OFF") == 0){
-      Serial.println("Relay ON");
-      digitalWrite(RELAY_PIN, HIGH);
-      digitalWrite(LED_PIN, LOW); // LED is active LOW
-      relay_state = "ON";
-    }
-    else {
-      Serial.println("Relay OFF");
-      digitalWrite(RELAY_PIN, LOW);
-      digitalWrite(LED_PIN, HIGH); // LED is active LOW
-      relay_state = "OFF";
-    }
-  }
+  // Parsing payload as JSON
+  StaticJsonDocument<200> inbound_JSON_message;
+  deserializeJson(inbound_JSON_message, payload);
 
-  Serial.println("MQTT publish of relay state");
-  MQTT_client.publish(MQTT_STATUS_TOPIC, MQTT_QOS, MQTT_RETAIN, relay_state);
-}
+  // Extracting the command state from payload
+  // Normally a const char* but copied using strdup so as to turn it into a char* for manipulation with strlwr
+  char* command_state = strdup(inbound_JSON_message["state"]);
 
-void MQTT_publish_callback(uint16_t packetId) {
-  Serial.println("MQTT published successfully");
+  // Reacting accordingly
+  if(strcmp(strlwr(command_state), "on") == 0) turn_relay_on();
+  else if(strcmp(strlwr(command_state), "off") == 0) turn_relay_off();
+  else if(strcmp(strlwr(command_state), "toggle") == 0) toggle_relay(); 
+
+  // Free memory
+  free(command_state);
 }
